daa65baf363ad5e10a48267b898525584e17e905
mostafakavand <mk323mk323mk32@gmail.com>
2024-04-17 06:56:04 -0400

    cleanup 'start and admin.py'

--- start and admin.py
+++ start and admin.py
@@ -2,9 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message , InlineKeyboardButton , InlineKeyboardMarkup
 from tabulate import tabulate
 import pandas as pd
-import os
-import time
-import finpy_tse as fpy
+
 
 
 
@@ -13,61 +11,59 @@ api_hash = "2076532de16fc82d242fcc1a012ce5f1"
 bot_token = "6872044004:AAETNHH9kO-XnzfyeIIq1oTRNQNN4lnNr2Y"
 
 
-# تعریف آرگومان های مورد نیاز
-bot = Client("mtest bot" ,
+#Define required arguments
+bot = Client("start bot" ,
             api_id = api_id ,
             api_hash = api_hash , 
             bot_token = bot_token ,
-            # proxy= proxy1
-
 )
 
-
-user_data = {}
-admin_info = {}
-
-# لیست مجاز chat_id های ادمین
-admin_chat_ids = {
-    181122579, # مثال: شماره تلفن ادمین را به عنوان chat_id در نظر بگیرید
-    # اضافه کردن بیشتر chat_id های ادمین در اینجا
+# Allowed list of admin usernames
+admin_username = {
+    'mostafa13438',
 }
 
-# تعریف دستور start
+# Definition of the start command
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot , message):
     first_name= message.from_user.first_name
     bot.send_message(message.chat.id, f"سلام {first_name}! به ربات خوش اومدی.")
 
-# تعریف پنل کاربری
+# Admin panel definition
 @bot.on_message(filters.command('admin') & filters.private)
 def admin_command(client, message):
     
-    chat_id = message.chat.id
+    username = message.from_user.username
     
-    if chat_id in admin_chat_ids: # اگر chat_id مورد نظر در لیست ادمین بود، دسترسی به دستورات مدیریتی را به کاربر می‌دهیم
+    if username in admin_username: # If the desired username was in the admin list, we give the user access to administrative commands
         
         keyboard = [
             [InlineKeyboardButton("دریافت اطلاعات مشتریان", callback_data='get_user_info')]
         ]
         reply_mark = InlineKeyboardMarkup(keyboard)
         message.reply_text("به پنل مدیریتی خوش آمدید", reply_markup=reply_mark)
-    else: # اگر chat_id مورد نظر در لیست ادمین نبود، پیام خطا را به کاربر می‌فرستیم
+    else: # If the desired username is not in the admin list, we will send an error message to the user
         
         message.reply_text("شما دسترسی به این دستور ندارید.")
 
-# ارسال لیست مشتریان برای ادمین
+# Send customer list to admin
 @bot.on_callback_query(filters.regex('^get_user_info$'))
 def get_user_info(client, callback_query):
     try:
-        # خواندن اطلاعات مشتریان از فایل اکسل
-        df = pd.read_excel('users.xlsx')
-        # تنظیم فرمت خروجی با استفاده از tabulate
-        user_info_text = tabulate(df, headers='keys', tablefmt='grid', numalign="right", stralign="center")
-        callback_query.message.reply_text(user_info_text)
-
-        # ارسال فایل اکسل به کاربر
+        # Excel file path
         file_path = 'users.xlsx'
+
+        # Reading customer information from an Excel file
+        df = pd.read_excel(file_path ,usecols= ['name', 'phone', 'email'])
+
+
+        # Set the output format using tabulate
+        user_info_text = tabulate(df, headers=['name', 'phone', 'email'], tablefmt="grid")
+
+        callback_query.message.edit_text(user_info_text)
+        # Send the Excel file to the user
         client.send_document(chat_id=callback_query.message.chat.id, document=file_path)
+
 
     except Exception as e:
         callback_query.message.reply_text(f"خطایی رخ داد: {e}")
